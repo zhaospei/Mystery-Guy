453128946a8c45222e6cd92983bc4dcb9577c7ee
Wunji <dungbuit1k28@gmail.com>
2022-03-15 09:08:41 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <SDL.h>
-#include <stdio.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <iostream>
 
-int main( int argc, char* args[] ){
-	printf("Hello World!");
-	printf("Update 2.0");
+bool init(){
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+	if (!(IMG_Init(IMG_INIT_PNG)))
+		std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+	if (!(TTF_Init()))
+		std::cout << "TTF_init has failed. Error: " << SDL_GetError() << std::endl;
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	return true;
+}
+
+int main(int argc, char* args[]){
+	std::cout << "Hello SDL";
+
 	return 0;
-
 }
